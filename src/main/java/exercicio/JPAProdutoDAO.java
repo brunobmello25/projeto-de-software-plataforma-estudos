5adcc03cb3fec7da348edcda9bc49bc3955af5ef
Bruno Mello <bruno.barros.mello@gmail.com>
2022-04-10 11:24:04 -0300

    initial commit

---
+++ src/main/java/exercicio/JPAProdutoDAO.java
@@ -0,0 +1,151 @@
+package exercicio;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.LockModeType;
+
+public class JPAProdutoDAO implements ProdutoDAO {
+	public long inclui(Produto umProduto) {
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = FabricaDeEntityManager.criarSessao();
+			tx = em.getTransaction();
+			tx.begin();
+
+			em.persist(umProduto);
+
+			tx.commit();
+
+			return umProduto.getId();
+		} catch (RuntimeException e) {
+			if (tx != null) {
+				try {
+					tx.rollback();
+				} catch (RuntimeException he) {
+				}
+			}
+			throw e;
+		} finally {
+			em.close();
+		}
+	}
+
+	public void altera(Produto umProduto) throws ProdutoNaoEncontradoException
+	{	
+		EntityManager em = null;
+		EntityTransaction tx = null;
+		Produto produto = null;
+		try
+		{	
+			em = FabricaDeEntityManager.criarSessao();
+			tx = em.getTransaction();
+			tx.begin();
+			
+			// lock desse registro na tabela de produto pra impedir que
+			// ele seja deletado por outro usuario
+			produto = em.find(Produto.class, umProduto.getId(), LockModeType.PESSIMISTIC_WRITE);
+			
+			if(produto == null)
+			{
+				tx.rollback();
+				throw new ProdutoNaoEncontradoException("Produto não encontrado");
+			}
+			
+			em.merge(umProduto);
+			tx.commit();
+		} 
+		catch(RuntimeException e)
+		{ 
+			if (tx != null)
+		    {   
+				try
+		        {	tx.rollback();
+		        }
+		        catch(RuntimeException he)
+		        { }
+		    }
+		    throw e;
+		}
+		finally
+		{   em.close();
+		}
+	}
+
+	public void exclui(long numero) throws ProdutoNaoEncontradoException 
+	{	EntityManager em = null;
+		EntityTransaction tx = null;
+		
+		try
+		{	
+			em = FabricaDeEntityManager.criarSessao();
+			tx = em.getTransaction();
+			tx.begin();
+
+			Produto produto = em.find(Produto.class, Long.valueOf(numero), LockModeType.PESSIMISTIC_WRITE);
+			
+			if(produto == null)
+			{	tx.rollback();
+				throw new ProdutoNaoEncontradoException("Produto não encontrado");
+			}
+
+			em.remove(produto);
+			tx.commit();
+		} 
+		catch(RuntimeException e)
+		{   
+			if (tx != null)
+		    {   
+				try
+		        {	tx.rollback();
+		        }
+		        catch(RuntimeException he)
+		        { }
+		    }
+		    throw e;
+		}
+		finally
+		{   em.close();
+		}
+	}
+
+	public Produto recuperaUmProduto(long numero) throws ProdutoNaoEncontradoException {
+		EntityManager em = null;
+
+		try {
+			em = FabricaDeEntityManager.criarSessao();
+
+			Produto umProduto = em.find(Produto.class, numero);
+
+			// Características no método find():
+			// 1. É genérico: não requer um cast.
+			// 2. Retorna null caso a linha não seja encontrada no banco.
+
+			if (umProduto == null) {
+				throw new ProdutoNaoEncontradoException("Produto não encontrado");
+			}
+
+			return umProduto;
+		} finally {
+			em.close();
+		}
+	}
+
+	public List<Produto> recuperaProdutos() {
+		EntityManager em = null;
+
+		try {
+			em = FabricaDeEntityManager.criarSessao();
+
+			@SuppressWarnings("unchecked")
+			List<Produto> produtos = em.createQuery("select p from Produto p order by p.id").getResultList();
+
+			return produtos;
+		} finally {
+			em.close();
+		}
+	}
+}
